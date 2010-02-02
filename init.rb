e566d3b77d29f9e0d06ab08973f6f6925a2e2a75
Elena Kasyanova <e.kasyanova@scalaxy.ru>
2010-02-02 13:05:10 +0300

    Counter & limit for each taskboard column

--- init.rb
+++ init.rb
@@ -19,6 +19,7 @@ Redmine::Plugin.register :redmine_task_board do
   project_module :task_boards do  
     permission :view_task_boards, :task_boards => :show
     permission :update_task_boards, :task_boards => :update_status
+    permission :edit_task_boards, :limits => :edit
   end
   
   menu :project_menu, :task_board, {:controller => 'task_boards', :action => 'show'}, :before => :burndown, :caption => 'Task Board'
